aa308b27323402893cbb2d9a93143aa5506522f5
Eloverflow <jfortin-moreau@mirageflow.com>
2019-04-20 09:20:31 -0400

    * change app name + add btn Ip Address

--- app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
+++ app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
@@ -1,28 +1,36 @@
 package io.github.controlwear.joystickdemo;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 public class MainActivity extends AppCompatActivity {
+    SocketClient socketClient;
     TextView connectionStateText;
     JoystickView joystickLeft;
     JoystickView joystickRight;
     Button resetSwitch;
+    Button changeIpBtn;
+    EditText txtUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtUrl = new EditText(this);
 
-        SocketClient socketClient = new SocketClient();// initialize();
+        socketClient = new SocketClient();// initialize();
         socketClient.execute();
 
 
@@ -56,19 +64,59 @@ public class MainActivity extends AppCompatActivity {
 
 
         resetSwitch = (Button) findViewById(R.id.reset_switch);
-
         resetSwitch.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new CmdExecution().execute(0, 0, 0, 0);
             }
         });
+
+
+
+
+        changeIpBtn = (Button) findViewById(R.id.change_ip_btn);
+        changeIpBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set the default text to a link of the Queen
+                txtUrl.setHint(ConfigurationService.getRobotIpAddress());
+                displayIpModal();
+            }
+        });
+    }
+
+    public void displayIpModal(){
+        if(txtUrl.getParent() != null) {
+            ((ViewGroup)txtUrl.getParent()).removeView(txtUrl); // <- fix
+        }
+
+        AlertDialog.Builder ipAddressDialog;
+
+        ipAddressDialog = new AlertDialog.Builder(this)
+                .setTitle("IP Address")
+                .setMessage("Please specify robot ip address")
+                .setView(txtUrl)
+                .setPositiveButton("Apply", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String ipAddress = txtUrl.getText().toString();
+                        ConfigurationService.setRobotIpAddress(ipAddress);
+
+                        socketClient.closeSocket();
+                        socketClient = new SocketClient();// initialize();
+                        socketClient.execute();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                });
+        ipAddressDialog.show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        SocketClient.closeSocket();
+        socketClient.closeSocket();
     }
 
 
